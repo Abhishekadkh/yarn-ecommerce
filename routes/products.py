92ee6a9a9128ac414c5a070a5fa3567ff4ba68b9
Abhishek Adhikari <abhishekkadh@gmail.com>
2025-07-21 18:51:26 +0545

    Register product router in main app

---
+++ routes/products.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException
+from schemas.product import Product
+from typing import List
+from uuid import uuid4
+
+router = APIRouter(prefix="/products", tags=["Products"])
+
+# In-memory list to simulate DB
+products = []
+
+@router.get("/", response_model=List[Product])
+def get_all_products():
+    return products
+
+@router.post("/", response_model=Product)
+def create_product(product: Product):
+    product.id = str(uuid4())  # Add a random ID
+    products.append(product)
+    return product
+
+@router.get("/{product_id}", response_model=Product)
+def get_product(product_id: str):
+    for product in products:
+        if product.id == product_id:
+            return product
+    raise HTTPException(status_code=404, detail="Product not found")
+
+@router.delete("/{product_id}")
+def delete_product(product_id: str):
+    global products
+    products = [p for p in products if p.id != product_id]
+    return {"message": "Product deleted"}
